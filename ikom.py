8ca3b43f1c21f11201e754b9972f60a60b6da7d4
lolocaha <sanssansanyc@gmail.com>
2026-01-15 10:59:56 +0500

    Update ikom.py

--- ikom.py
+++ ikom.py
@@ -1,4 +1,6 @@
 import csv
+import os
+
 owners= []
 def test():#Функция для проверки csv файла и вывода его
     try:
@@ -32,20 +34,24 @@ def bubble_sort(arr,zad): #сортировка пузырьком
     return arr
 def report1(owners):#Меню для 1 задания
     print('1.Все автовладельцы')
-    print('2Сначала сортировка по году постановки на учет (по убыванию) , а затем по фамилиям (по возрастанию)')
     def task1(a,b):
         if a['Год постановки на учёт'] != b['Год постановки на учёт']: #если значение не равны, то сортируем по убыванию
             return a['Год постановки на учёт'] < b['Год постановки на учёт']
         return a['Фамилия'] > b['Фамилия']#сортируем по возрастанию
     sorted_owners = owners.copy()#копируем список
     bubble_sort(sorted_owners,task1) #сортируем список
-    for row in sorted_owners: #проходим по нему и выводим значения
-        print(row)
+    with open('all_owners.csv',mode='w',newline='')as file:
+        writer=csv.writer(file)
+        for row in sorted_owners: #проходим по нему и выводим значения
+            firsttask = []
+            firsttask.append(row)
+            writer.writerow(firsttask)
+        if os.path.exists('all_owners.csv'):
+            print('Файл all_owners создан')
     print('\n')
 
 def report2(owners): #Меню для 2 задания
     print('2.Все владельцы автомобиля "ВАЗ"')
-    print('Сначала сортировка по году выпуска (по убыванию), потом по объему двигателя(по возрастанию), а затем по фамилии(по возрастанию)')
     vaz_owners = [] #Создаем список куда будут записываться люди с ВАЗ
     for row in owners:
         brand = row['Марка автомобиля']#находим строки, в которых есть автомобиль ВАЗ
@@ -61,14 +67,18 @@ def report2(owners): #Меню для 2 задания
     if len(vaz_owners)  == 0:#если список пустой, то выведет, что таких автомобилистов нет
         print('Людей с таким автомобилем нет')
     else:
-        bubble_sort(vaz_owners,task2)#сортируем и выводим значени
-        for row in vaz_owners:
-            print(row)
-print('\n')
+        bubble_sort(vaz_owners,task2)#сортируем и выводим значение
+        with open('VAZ_owners', mode='w', newline='') as fi:
+            writer=csv.writer(fi)
+            for row in vaz_owners:
+                secondtask = []
+                secondtask.append(row)
+                writer.writerow(secondtask)
+            if os.path.exists('VAZ_owners.csv'):
+                print('Файл VAZ_owners.csv создан')
+
 def report3(owners):
     print('3.Список всех владельцев автомобилей с годом выпуска ранее заданного')
-    print('Сортировка по году выпуска (по убыванию) и по  марке автомобиля (по возрастанию)')
-
     year = input('Введите год:')#вводим год и проверяем является ли введенное значение числом
     if year.isdigit():
         limit_year = int(year)
@@ -88,8 +98,15 @@ def report3(owners):
         print('Таких владельцев нет')
     else:
         bubble_sort(old_cars,task3)#сортируем и выводим значения
-        for i in old_cars:
-            print(i)
+        with open('Old_cars_owners.csv', mode='w', newline='') as fil:
+            writer=csv.writer(fil)
+            for i in old_cars:
+                old_cars_sort= []
+                old_cars_sort.append(i)
+                writer.writerow(old_cars_sort)
+            if os.path.exists('Old_cars_owners.csv'):
+                print('Файл Old_cars_owners.csv создан')
+
 def main(): #Функция для интерфейса меню
     all_owners = test()
     if len(all_owners) < 25:
@@ -111,6 +128,7 @@ def main(): #Функция для интерфейса меню
             break
         else:
             print('Введите значение заново')
+        print(input('Для продолжения нажмите Enter'))
 if __name__ == '__main__':
     main()
 
